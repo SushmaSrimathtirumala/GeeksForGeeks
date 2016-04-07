124f5d966678753ce6e0663f75c3d06a8f840b55
gopalakshintala <gopalakshintala@gmail.com>
2016-04-07 15:42:28 +0530

    commit

---
+++ src/BackTracking/RatMaze.java
@@ -0,0 +1,42 @@
+package BackTracking;
+
+import java.util.Arrays;
+
+/**
+ * Created by gakshintala on 4/7/16.
+ */
+public class RatMaze {
+    public static void main(String[] args) {
+        int[][] maze  =  { {1, 0, 0, 0}, 
+                {1, 1, 0, 1},
+                {0, 1, 0, 0},
+                {1, 1, 1, 1}
+        };
+        boolean[][] visited = new boolean[4][4];
+        if(solveMaze(maze, 0, 0, visited)) {
+            System.out.println("Rat reached the end! :)");
+            for(boolean[] path: visited) {
+                System.out.println(Arrays.toString(path));
+            }
+        } else {
+            System.out.println("Rat can't be out :(");
+        }
+    }
+
+    private static boolean solveMaze(int[][] maze, int i, int j, boolean[][] visited) {
+        boolean isSolved = false;
+        if(i==3 && j==3) return true; // Checking base case that Rat reached end
+        if(isValidMove(i,j, maze)) {
+            visited[i][j] = true;
+            isSolved = solveMaze(maze, i+1, j, visited) || solveMaze(maze,i,j+1, visited);
+            visited[i][j] = isSolved; // Backtracking, it will go to parent call and trigger the next recursive call 
+            // after OR. If both are done, they shall go to their parent and so on
+        }
+        return isSolved;
+    }
+
+    private static boolean isValidMove(int i, int j, int[][] maze) {
+        return i>=0 && i<4 && j>=0 && j<4 && maze[i][j]!=0;
+    }
+
+}
---
+++ src/DP/SubSetWithEqualSum/SubSetWithEqualSum.java
@@ -0,0 +1,49 @@
+package DP.SubSetWithEqualSum;
+
+/**
+ * Created by gakshintala on 3/21/16.
+ */
+public class SubSetWithEqualSum {
+    public static void main(String[] args) {
+        int[] set = {1, 6, 11, 5};
+        System.out.println(findMinDiff(set));
+    }
+
+    private static int findMinDiff(int[] set) {
+        int sum = findSum(set);
+        int len = set.length;
+        boolean[][] table = new boolean[sum + 1][len + 1];
+        for (int i = 0; i <= len; i++) {
+            table[0][i] = true;
+        }
+
+        for (int i = 1; i <= sum; i++) {
+            for (int j = 1; j <= len; j++) {
+                table[i][j] = table[i][j - 1];
+                if (set[j - 1] <= i) {
+                    table[i][j] = table[i][j] || table[i - set[j - 1]][j];
+                }
+            }
+        }
+
+        return searchMiniff(table, sum, len);
+    }
+
+    private static int searchMiniff(boolean[][] table, int sum, int len) {
+        int j = len;
+        for (int i = sum / 2; i >= 0; i--) {
+            if (table[i][j] == true) {
+                return sum - 2 * i; // Sum of one subset is i and other is sum-i, so its (Sum-i) - i 
+            }
+        }
+        return Integer.MAX_VALUE; // no way this comes here
+    }
+
+    private static int findSum(int[] set) {
+        int sum = 0;
+        for (int i : set) {
+            sum += i;
+        }
+        return sum;
+    }
+}
